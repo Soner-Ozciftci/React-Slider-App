3b156bed8dcbbcc4021375b70fd4674dbb54398a
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-31 23:24:48 +0200

    img src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-
+import React from "react";
+import Review from './Review';
 
 function App() {
   return (
  <main>
-  
+<section className="container">
+<div className="title">
+<h2>Our Characters
+<div className="underline">
+</div>
+</h2>
+</div>
+<Review/>
+</section>
  </main>
   );
 }
---
+++ src/Review.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import people from "./data";
+import { faChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+
+function Review() {
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src="https://upload.wikimedia.org/wikipedia/tr/0/03/Walter_White_S5B.png" />
+      </div>
+    </article>
+  );
+}
+
+export default Review;
