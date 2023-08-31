73d7903d414f77719e1964756abf9df1fb9e1a4d
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-31 23:37:23 +0200

    prev-next

--- src/Review.js
+++ src/Review.js
@@ -6,7 +6,20 @@ function Review() {
   return (
     <article className="review">
       <div className="img-container">
-        <img src="https://upload.wikimedia.org/wikipedia/tr/0/03/Walter_White_S5B.png" />
+        <img src="https://upload.wikimedia.org/wikipedia/tr/0/03/Walter_White_S5B.png" alt="" />
+      </div>
+      <div className="name-job">
+        <h4 className="name">Walter White</h4>
+        <p className="job">Meth Manufacturer</p>
+      </div>
+      <p className="info">text</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <faChevronCircleLeft/>
+        </button>
+        <button className="next-btn">
+          <faChevronCircleRight/>
+        </button>
       </div>
     </article>
   );
